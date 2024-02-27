eceb1cfab4c919370464dd8c3fca56a1224d323c
Dan Noonan <dnoonan23@yahoo.com>
2024-02-26 22:57:48 -0600

    another routing change

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const html = require('express').Router();
-//Path for homepage
-html.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-});
+
 //Path for notes page
 html.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+//Path for homepage
+html.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
 module.exports = html;
